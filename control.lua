8f61c66eb1d553a6f4c2b13f5b36b6688c6bf83d
billbo99 <cactus_computers-github@yahoo.com>
2023-11-15 19:36:07 +0000

    1.0.8

--- control.lua
+++ control.lua
@@ -1,4 +1,9 @@
 local Smarts = require("smarts")
+local Gui = require("scripts.gui")
+
+local function OnGuiClosed(event)
+    Gui.OnGuiClosed(event)
+end
 
 script.on_init(Smarts.on_init)
 script.on_load(Smarts.on_load)
@@ -7,3 +12,4 @@ script.on_event(defines.events.on_player_created, Smarts.on_player_created)
 script.on_event(defines.events.on_player_joined_game, Smarts.on_player_joined_game)
 script.on_event(defines.events.on_gui_click, Smarts.on_gui_click)
 script.on_event(defines.events.on_player_died, Smarts.on_player_died)
+script.on_event(defines.events.on_gui_closed, OnGuiClosed)
--- scripts/gui.lua
+++ scripts/gui.lua
@@ -21,46 +21,56 @@ function Gui.CreateTopGui(player)
     if not button then
         button =
             button_flow.add {
-            type = "sprite-button",
-            name = "DeathCounterMainButton",
-            sprite = "DeathCounter",
-            style = mod_gui.button_style
-        }
+                type = "sprite-button",
+                name = "DeathCounterMainButton",
+                sprite = "DeathCounter",
+                style = mod_gui.button_style
+            }
     end
     return button
+end
+
+function Gui.OnGuiClosed(e)
+    if not e.element then return end
+    if e.element and e.element.get_mod() ~= "Death_Counter" then return end
+    local player = game.get_player(e.player_index)
+    player.opened = nil
+    player.gui.left["DeathCounterMain"].destroy()
 end
 
 function Gui.CreateMainGui(player)
     if player.gui.left["DeathCounterMain"] then
         player.gui.left["DeathCounterMain"].destroy()
+        player.opened = nil
     end
 
     if global.causes and Utils.TableSize(global.causes) > 0 then
-        local DeathCounterMain = player.gui.left.add({type = "frame", name = "DeathCounterMain", direction = "vertical"})
+        local DeathCounterMain = player.gui.left.add({ type = "frame", name = "DeathCounterMain", direction = "vertical" })
+        player.opened = DeathCounterMain
         DeathCounterMain.style.minimal_height = 10
         DeathCounterMain.style.minimal_width = 10
 
-        DeathCounterMain.add({type = "label", caption = "Death Counter", style = "heading_1_label"})
+        DeathCounterMain.add({ type = "label", caption = "Death Counter", style = "heading_1_label" })
 
         local headers = Utils.sortedKeys(global.causes)
         local column_count = 2 + #headers
-        local f = DeathCounterMain.add({type = "table", style = "mod_info_table", column_count = column_count})
+        local f = DeathCounterMain.add({ type = "table", style = "mod_info_table", column_count = column_count })
 
-        f.add({type = "label", caption = "Player", style = "bold_label"})
-        f.add({type = "label", caption = "Total", style = "bold_label"})
+        f.add({ type = "label", caption = "Player", style = "bold_label" })
+        f.add({ type = "label", caption = "Total", style = "bold_label" })
         for _, header in pairs(headers) do
             if header == "Suicide" then
-                f.add({type = "label", caption = "Suicide"})
+                f.add({ type = "label", caption = "Suicide" })
             elseif header == "Unknown" then
-                f.add({type = "label", caption = "Unknown"})
+                f.add({ type = "label", caption = "Unknown" })
             elseif Utils.starts_with(header, "player") then
                 local killer = Utils.split(header, "/")[2]
-                f.add({type = "sprite", sprite = "entity.character", tooltip = killer})
+                f.add({ type = "sprite", sprite = "entity.character", tooltip = killer })
             else
                 if player.gui.is_valid_sprite_path(string.format("entity.%s", header)) then
-                    f.add({type = "sprite", sprite = string.format("entity.%s", header), tooltip = {"entity-name." .. header}})
+                    f.add({ type = "sprite", sprite = string.format("entity.%s", header), tooltip = { "entity-name." .. header } })
                 else
-                    f.add({type = "sprite", sprite = "utility.questionmark", tooltip = header})
+                    f.add({ type = "sprite", sprite = "utility.questionmark", tooltip = header })
                 end
             end
         end
@@ -77,13 +87,13 @@ function Gui.CreateMainGui(player)
 
         local rank = Utils.sortedKeys(death_counts)
         for _, player_name in pairs(rank) do
-            f.add({type = "label", caption = player_name})
-            f.add({type = "label", caption = death_counts[player_name]})
+            f.add({ type = "label", caption = player_name })
+            f.add({ type = "label", caption = death_counts[player_name] })
             for _, header in pairs(headers) do
                 if global.players[player_name]["DeathCount"][header] then
-                    f.add({type = "label", caption = global.players[player_name]["DeathCount"][header]})
+                    f.add({ type = "label", caption = global.players[player_name]["DeathCount"][header] })
                 else
-                    f.add({type = "label", caption = ""})
+                    f.add({ type = "label", caption = "" })
                 end
             end
         end
